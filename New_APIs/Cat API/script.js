e44808865e1268132454446aba280df2901bf66a
Ananya Gupta <145869907+ananyag309@users.noreply.github.com>
2024-06-11 20:44:43 +0530

    Update script.js
    added api key

--- New_APIs/Cat API/script.js
+++ New_APIs/Cat API/script.js
@@ -1,11 +1,15 @@
 document.getElementById('fetchCat').addEventListener('click', fetchCatImage);
 
 function fetchCatImage() {
-    fetch('https://api.thecatapi.com/v1/images/search')
-        .then(response => response.json())
-        .then(data => {
-            const catImageContainer = document.getElementById('catImageContainer');
-            catImageContainer.innerHTML = `<img src="${data[0].url}" alt="Random Cat Image">`;
-        })
-        .catch(error => console.error('Error fetching cat image:', error));
+    fetch('https://api.thecatapi.com/v1/images/search', {
+        headers: {
+            'x-api-key': 'live_ce4pjoxHExOT6cXQMjajh6ILE7xlDZIc1BsaIAY1Gu01tYArMy64ngRdq2Eu0fAe'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const catImageContainer = document.getElementById('catImageContainer');
+        catImageContainer.innerHTML = `<img src="${data[0].url}" alt="Random Cat Image">`;
+    })
+    .catch(error => console.error('Error fetching cat image:', error));
 }
