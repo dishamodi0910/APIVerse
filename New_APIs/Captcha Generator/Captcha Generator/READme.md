b515e883f1cf0d101207b75106ef1cde5581cd80
Pradnya Gaitonde <116059908+PradnyaGaitonde@users.noreply.github.com>
2024-07-26 23:14:29 +0530

    Custom Captcha Generator
    The Custom Captcha Generator is a security tool designed to create unique and unpredictable tests to differentiate between human users and automated bots. This project is developed using HTML, CSS, and JavaScript, ensuring a seamless integration into web applications with a focus on user-friendly design and robust functionality.

---
+++ New_APIs/Captcha Generator/Captcha Generator/READme.md
@@ -0,0 +1,43 @@
+# Captcha Generator
+
+## Overview
+The Captcha Generator is a security tool designed to create unique and unpredictable tests to differentiate between human users and automated bots. This project is developed using HTML, CSS, and JavaScript, ensuring a seamless integration into web applications with a focus on user-friendly design and robust functionality.
+
+## Features
+* HTML Structure: Clear and semantic structure for Captcha elements.
+* CSS Styling: Visually appealing and accessible design, with responsive layouts.
+* JavaScript Functionality: Handles random challenge generation, user response validation, and feedback.
+
+## Installation
+* Clone the repository:
+
+```bash
+git clone https://github.com/yourusername/captcha-generator.git
+``` 
+
+* Navigate to the project directory:
+
+```bash
+cd captcha-generator
+```
+
+* Open index.html in your preferred web browser.
+
+## Usage
+* Generate Captcha: When the page loads, a new Captcha challenge is automatically generated.
+* Solve Captcha: Enter your response in the input field provided.
+* Submit: Click the submit button to validate your response.
+* Feedback: You will receive immediate feedback indicating whether your response was correct or incorrect.
+
+## Project Structure
+
+```bash
+captcha-generator/
+│
+├── index.html      # Main HTML file
+├── style.css       # CSS file for styling
+├── script.js       # JavaScript file for functionality
+```
+## Screenshots
+
+![alt text](image.png)
---
+++ New_APIs/Captcha Generator/Captcha Generator/script.js
@@ -0,0 +1,57 @@
+// Selecting necessary DOM elements
+const captchaTextBox = document.querySelector(".captch_box input");
+const refreshButton = document.querySelector(".refresh_button");
+const captchaInputBox = document.querySelector(".captch_input input");
+const message = document.querySelector(".message");
+const submitButton = document.querySelector(".button");
+
+// Variable to store generated captcha
+let captchaText = null;
+
+// Function to generate captcha
+const generateCaptcha = () => {
+  const randomString = Math.random().toString(36).substring(2, 7);
+  const randomStringArray = randomString.split("");
+  const changeString = randomStringArray.map((char) => (Math.random() > 0.5 ? char.toUpperCase() : char));
+  captchaText = changeString.join("   ");
+  captchaTextBox.value = captchaText;
+  console.log(captchaText);
+};
+
+const refreshBtnClick = () => {
+  generateCaptcha();
+  captchaInputBox.value = "";
+  captchaKeyUpValidate();
+};
+
+const captchaKeyUpValidate = () => {
+  //Toggle submit button disable class based on captcha input field.
+  submitButton.classList.toggle("disabled", !captchaInputBox.value);
+
+  if (!captchaInputBox.value) message.classList.remove("active");
+};
+
+// Function to validate the entered captcha
+const submitBtnClick = () => {
+  captchaText = captchaText
+    .split("")
+    .filter((char) => char !== " ")
+    .join("");
+  message.classList.add("active");
+  // Check if the entered captcha text is correct or not
+  if (captchaInputBox.value === captchaText) {
+    message.innerText = "Entered captcha is correct";
+    message.style.color = "#826afb";
+  } else {
+    message.innerText = "Entered captcha is not correct";
+    message.style.color = "#FF2525";
+  }
+};
+
+// Add event listeners for the refresh button, captchaInputBox, submit button
+refreshButton.addEventListener("click", refreshBtnClick);
+captchaInputBox.addEventListener("keyup", captchaKeyUpValidate);
+submitButton.addEventListener("click", submitBtnClick);
+
+// Generate a captcha when the page loads
+generateCaptcha();
